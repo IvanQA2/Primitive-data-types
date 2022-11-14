808b72747667034181c0f192d33fb40b4cf9023a
Ivan test <ivan02081983s@ya.ru>
2022-11-14 21:03:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price_tiket = 100;
+        int price_of_a_mile = 20;
+        int mile = price_tiket / price_of_a_mile;
+        System.out.println("Количество начисленных миль за купленный билет: " + mile);
+    }
+}
