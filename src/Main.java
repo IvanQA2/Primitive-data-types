73e834397561583afda8e5da65cd3d12d3123dae
Ivan test <ivan02081983s@ya.ru>
2022-11-22 08:08:43 +0300

    Camel case

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price_tiket = 100;
-        int price_of_a_mile = 20;
-        int mile = price_tiket / price_of_a_mile;
+        int priceTiket = 10000;
+        int priceOfAMmile = 20;
+        int mile = priceTiket / priceOfAMmile;
         System.out.println("Количество начисленных миль за купленный билет: " + mile);
     }
 }
